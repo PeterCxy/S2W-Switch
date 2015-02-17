254504c3640a1ddfd7175976f830d7da2ac02738
Peter Cai <xqsx43cxy@126.com>
2015-02-17 22:01:37 +0800

    Initial commit

---
+++ app/src/main/java/us/shandian/s2w/MainActivity.java
@@ -0,0 +1,14 @@
+package us.shandian.s2w;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
